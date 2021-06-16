c78ff6160454c28a99c770aad715a144c605f5ca
yuryofujita <dasman74@hotmail.com>
2021-06-17 05:23:44 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -11,10 +11,24 @@ import React, {Component} from "react";
 //     "Hello, world!!"
 //   );
 // }
-function App() {
-  const greeting = "Hi! Tom";
-  const dom = <h1 className= "foo">{ greeting }</h1>
-  return dom;
+// function App() {
+//   const greeting = "Hi! Tom";
+//   const dom = <h1 className= "foo">{ greeting }</h1>
+//   return dom;
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
