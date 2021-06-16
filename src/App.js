aef742864d3b1e304e2f34fe3bf40cb5d3c47590
yuryofujita <dasman74@hotmail.com>
2021-06-17 06:03:50 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, {Component} from "react";
-// function App() {
-//   return (
-//     <div>Hello World!</div>
-//   );
-// }
-// function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "Hello, world!!"
-//   );
-// }
-// function App() {
-//   const greeting = "Hi! Tom";
-//   const dom = <h1 className= "foo">{ greeting }</h1>
-//   return dom;
-// }
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname" },
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am { props.name }! and {props.age} years old. </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
