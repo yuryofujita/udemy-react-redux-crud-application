20ffcfbe9bc11a3c525059a4da0ef4609f7f8a08
yuryofujita <dasman74@hotmail.com>
2021-06-18 06:48:59 +0900

    implement component witch connect function

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from "../actions";
+
+class App extends Component {
+  render() {
+    const props = this.props
+    return (
+      <React.Fragment>
+        <div>counter:{props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement()),
+// })
+const mapDispatchToProps = ({ increment, decrement })
+export default connect(mapStateToProps, mapDispatchToProps)(App)
